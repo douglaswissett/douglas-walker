6350a8b782da4cb2c543f5c76ea4a2e3cb342c42
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-04-29 13:04:32 +0200

    more boilerplate

--- js/script.js
+++ js/script.js
@@ -7,5 +7,21 @@ $(document).ready(() => {
   console.log('js loaded');
 
 
+  // fiddle to hide nav bar
+  (function($) {
+    var ost = 0;
+    $(window).scroll(function() {
+      var cOst = $(this).scrollTop();
+
+      if(cOst > ost) {
+         $('nav').addClass('fixed').removeClass('default');
+      }
+      else {
+         $('nav').addClass('default').removeClass('fixed');
+      }
+
+      ost = cOst;
+    });
+  })(jQuery);
 
 })
