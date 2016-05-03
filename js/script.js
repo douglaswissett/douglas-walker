59f300ef0c379bb8474bff7a8d6befac4f1116dc
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-05-03 11:03:09 +0200

    headroom working

--- js/script.js
+++ js/script.js
@@ -6,37 +6,13 @@
 $(document).ready(() => {
   console.log('js loaded');
 
-
-  // // fiddle to hide nav bar
-  // (function($) {
-  //   var ost = 0;
-  //   $(window).scroll(function() {
-  //     var cOst = $(this).scrollTop();
-
-  //     if(cOst > ost) {
-  //        $('nav').addClass('fixed').removeClass('default');
-  //     }
-  //     else {
-  //        $('nav').addClass('default').removeClass('fixed');
-  //     }
-
-  //     ost = cOst;
-  //   });
-  // })(jQuery);
-
+  
   // grab an element
-  var elem = document.querySelector("#header");
+  var myElement = document.querySelector("#header");
   // construct an instance of Headroom, passing the element
-  var headroom = new Headroom(elem, {
-    "offset": 205,
-    "tolerance": 5,
-    "classes": {
-      "initial": "animated",
-      "pinned": "slideDown",
-      "unpinned": "slideUp"
-    }
-  });
-  headroom.init();
+  var headroom  = new Headroom(myElement);
+  // initialise
+  headroom.init(); 
 
 
 
