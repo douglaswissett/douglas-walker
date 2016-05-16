d4cadd0afe6061a1dfa80f11617a75dcbdca3d85
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-05-16 15:39:09 +0200

    setting layout

--- js/script.js
+++ js/script.js
@@ -25,25 +25,25 @@ $(document).ready(() => {
   $('.aboutNav').click((e) => {
     e.preventDefault();
 
-    $('html, body').animate({scrollTop: $('#profile').offset().top }, 1500);
+    $('html, body').animate({scrollTop: $('#profile').offset().top - 20 }, 1500);
   });
 
   $('.workNav').click((e) => {
     e.preventDefault();
 
-    $('html, body').animate({scrollTop: $('#work').offset().top - 120}, 1500);
+    $('html, body').animate({scrollTop: $('#work').offset().top - 30 }, 1500);
   });
 
   $('.contactNav').click((e) => {
     e.preventDefault();
 
-    $('html, body').animate({scrollTop: $('#contact').offset().top - 120}, 1500);
+    $('html, body').animate({scrollTop: $('#contact').offset().top - 30 }, 1500);
   });
 
   $('#contactHere').click((e) => {
     e.preventDefault();
 
-    $('html, body').animate({scrollTop: $('#contactForm').offset().top - 120}, 1500);
+    $('html, body').animate({scrollTop: $('#contactForm').offset().top - 120 }, 1500);
   });
 
 
