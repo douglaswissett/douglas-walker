766404b1a729279c87bcb9cc6208f01c02de364d
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-05-21 13:26:09 +0200

    cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-/*
-  AUTHER: DOUGLAS WISSETT WALKER
-  TITLE:  PERSONAL SITE for douglas-walker.com
-*/
-
 $(document).ready(() => {
-  console.log('js loaded');
-
-
-
   /* Headroom.js code */
   // grab an element
   var myElement = document.querySelector("#header");
@@ -15,36 +6,21 @@ $(document).ready(() => {
   var headroom  = new Headroom(myElement);
   // initialise
   headroom.init(); 
-
-
-
-
-  
   // Navigation anchor links slow scroll animate on page
   $('.aboutNav').click((e) => {
     e.preventDefault();
-
     $('html, body').animate({scrollTop: $('#profile').offset().top - 20 }, 1500);
   });
-
   $('.workNav').click((e) => {
     e.preventDefault();
-
     $('html, body').animate({scrollTop: $('#work').offset().top - 30 }, 1500);
   });
-
   $('.contactNav').click((e) => {
     e.preventDefault();
-
     $('html, body').animate({scrollTop: $('#contact').offset().top - 30 }, 1500);
   });
-
   $('#contactHere').click((e) => {
     e.preventDefault();
-
     $('html, body').animate({scrollTop: $('#contactForm').offset().top - 120 }, 1500);
   });
-
-
-
 });
