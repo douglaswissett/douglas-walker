a435f3c55830732f02510105bc076d76201b41ba
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-05-26 19:09:38 +0200

    slider animation onLeave onEnter

--- js/script.js
+++ js/script.js
@@ -11,50 +11,86 @@ $(document).ready(() => {
     scrollBar: true,
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
     scrollingSpeed: 700,
+    loopHorizontal: false,
 
     afterLoad: function(anchorLink, index){
       var loadedSection = $(this);
-
       //using index
       if(index == 1) {
+        // hide fixed nav arrows  
+        $('.nav-arrow').fadeOut();
         setTimeout(() => {
           $('.kurama h1').addClass('animated flipInY');
         }, 200 );
-        // $('.float-1').addClass('animated bounceInLeft');
-        // $('.float-2').addClass('animated bounceInRight');
+        $('.float-1').addClass('animated bounceInLeft');
+        $('.float-2').addClass('animated bounceInLeft');
         $('.obito h1').addClass('animated bounceInLeft');
         $('.minato h1').addClass('animated bounceInLeft')
         $('#findMore').addClass('animated bounceInUp');
       }
       if(index == 2){
-        $('.about-col').show();
-        $('.about-col-header').show();
+        // show fixed nav arrows
+        $('.arrow-bottom').fadeIn();
+        $('.arrow-bottom').addClass('flipInX');
+        // animate.css
+        $('.about-col').fadeIn();
+        $('.about-col-header').fadeIn();
         $('.about-col').addClass('animated bounceInUp');
         $('.about-col-header').addClass('animated bounceInDown');
-        $('.about-btn').show();
-        $('.about-btn').addClass('animated bounceInDown');
-        // buggy hinge        
-        // setTimeout(() => {
-        //   $('.about-btn').removeClass('infinite bounce');
-        //   $('.about-btn').addClass('hinge');
-        //   setTimeout(() => {
-        //     $('.about-btn').removeClass('hinge');
-        //     $('.about-btn').hide();
-        //   },2000);
-        // },4000);
       }
       if(index == 3) {
-        setTimeout(() => {
-          $('#work h1').show();
-          $('#work h1').addClass('animated zoomIn');
+        // display fixed nav arrows
+        $('.nav-arrow').fadeIn();
+        $('.nav-arrow').addClass('flipInX')
+        // animate.css
+        setTimeout(() => { $('#work h1').fadeIn(); $('#work h1').addClass('animated zoomIn');
         }, 200 );
-        $('.row-design').show();
+        $('.row-design').fadeIn();
         $('.web-design').addClass('animated fadeInLeft');
         $('.section-image.design').addClass('animated bounceInLeft');
-        $('.row-dev').show();
+        $('.row-dev').fadeIn();
         $('.row-dev').addClass('animated rollIn');
       }
+      if(index == 4) {
+        // display fixed nav arrows
+        $('.nav-arrow').fadeIn();
+        $('.nav-arrow').addClass('flipInX')
+      }
+      if(index == 5) {
+        // display fixed nav arrows
+        $('.nav-arrow').fadeIn();
+        $('.nav-arrow').addClass('flipInX')
+      }
+      if(index == 6) {
+        $('.arrow-bottom').fadeOut();
+      }
+    },
+    onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
+      let leavingSlide = $(this);
+      //leaving the first slide of the 2nd Section to the right
+      if(index == 4 && slideIndex == 0 && direction == 'right'){
+        $('.special-line').removeClass('zoomInRight');
+        $('.special-line').addClass('zoomOutLeft');
+      }
+      //leaving the 3rd slide of the 2nd Section to the left
+      if(index == 4 && slideIndex == 1 && direction == 'left'){
+        $('.special-line').removeClass('zoomOutLeft');
+        $('.special-line').addClass('zoomInRight');
+      }
     }
+  });
+  // fullPage.js btn navigation
+  $('.arrow-top').click((e) => {
+    $.fn.fullpage.moveSectionUp();
+  });
+  $('.arrow-bottom').click((e) => {
+    $.fn.fullpage.moveSectionDown();
+  });
+  $('#findMore').click((e) => {
+    $.fn.fullpage.moveTo(2);
+  });
+  $('.about-btn').click((e) => {
+    $.fn.fullpage.moveTo(3);
   });
   /* Headroom.js code */
   // grab an element
@@ -66,23 +102,21 @@ $(document).ready(() => {
   // Navigation anchor links slow scroll animate on page
   $('.aboutNav').click((e) => {
     e.preventDefault();
-    $('html, body').animate({scrollTop: $('#profile').offset().top - 20 }, 800);
+    $.fn.fullpage.moveTo(2);
   });
   $('.workNav').click((e) => {
     e.preventDefault();
-    $('html, body').animate({scrollTop: $('#work').offset().top - 160 }, 800);
+    $.fn.fullpage.moveTo(4);
   });
   $('.contactNav').click((e) => {
     e.preventDefault();
-    $('html, body').animate({scrollTop: $('#contact').offset().top - 30 }, 800);
+    $.fn.fullpage.moveTo(5);
   });
   $('#contactHere').click((e) => {
     e.preventDefault();
-    $('html, body').animate({scrollTop: $('#contact').offset().top - 30 }, 800);
+    $.fn.fullpage.moveTo(5);
   });
-  $('#findMore').click((e) => {
-    $.fn.fullpage.moveTo(2);
-  });  
+  // Init Parallax scenes
   $('#scene').parallax();
   $('#scene2').parallax();
   // My work slideshow image cycles
